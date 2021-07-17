0de40a8a2b5c80f965abe13a427609277325f5cc
JONE <31957754+jonexyz@users.noreply.github.com>
2021-07-18 02:11:52 +0800

    Add files via upload

---
+++ src/wangEditorV4.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jonexyz\wangEditorV4;
+
+use Encore\Admin\Extension;
+
+class wangEditorV4 extends Extension
+{
+    public $name = 'wang-editor-v4';
+
+    public $views = __DIR__.'/../resources/views';
+
+    public $assets = __DIR__.'/../resources/assets';
+}
